5705fdd69c3472ac76c61b9bd43e0dd3728e4314
Esther Decatorsmith <100103525+estherdecatorsmith@users.noreply.github.com>
2022-03-26 14:32:33 -0500

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-length = float(input("Enter length of rectangle: "))
-width = float(input("Enter width of rectangle: "))
-
-area = length*width
-circumference = 2*(length) + 2*(width)
-
-print("Area of rectangle: ",area)
-print("Circumference of rectangle: ",circumference)
